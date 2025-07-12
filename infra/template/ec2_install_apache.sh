5526a361e1a7120de6117229d933c53051d8348f
plamba07 <prekshalamba1603@gmail.com>
2025-07-12 14:41:25 +0530

    Update ec2_install_apache.sh

--- infra/template/ec2_install_apache.sh
+++ infra/template/ec2_install_apache.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-set -e
 exec > /var/log/user-data.log 2>&1
+set -e
 
 cd /home/ubuntu
+
+# Install dependencies
 apt update -y
 apt install -y python3 python3-pip python3-venv git
 
-# Clone repo
+# Clone app
 git clone https://github.com/rahulwagh/python-mysql-db-proj-1.git
 cd python-mysql-db-proj-1
 
@@ -14,8 +16,8 @@ cd python-mysql-db-proj-1
 python3 -m venv venv
 source venv/bin/activate
 
-# Install dependencies
+# Install requirements
 venv/bin/pip install -r requirements.txt
 
-# Start app in background and log output
+# Start app
 nohup venv/bin/python3 app.py > app.log 2>&1 &
