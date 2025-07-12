d17ed7a197112aab9a3bd2a674b7156f890d8a8d
plamba07 <prekshalamba1603@gmail.com>
2025-07-12 13:43:51 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -37,7 +37,7 @@ module "ec2" {
 
 module "lb_target_group" {
   source                   = "./load-balancer-target-group"
-  lb_target_group_name     = "dev-proj-1-lb-target-group"
+  lb_target_group_name     = "dev-proj-2-lb-target-group"
   lb_target_group_port     = 5000
   lb_target_group_protocol = "HTTP"
   vpc_id                   = module.networking.dev_proj_1_vpc_id
@@ -46,12 +46,12 @@ module "lb_target_group" {
 
 module "alb" {
   source                    = "./load-balancer"
-  lb_name                   = "dev-proj-1-alb"
+  lb_name                   = "dev-proj-2-alb"
   is_external               = false
   lb_type                   = "application"
   sg_enable_ssh_https       = module.security_group.sg_ec2_sg_ssh_http_id
   subnet_ids                = tolist(module.networking.dev_proj_1_public_subnets)
-  tag_name                  = "dev-proj-1-alb"
+  tag_name                  = "dev-proj-2-alb"
   lb_target_group_arn       = module.lb_target_group.dev_proj_2_lb_target_group_arn
   ec2_instance_id           = module.ec2.dev_proj_1_ec2_instance_id
   lb_listner_port           = 5000
@@ -59,7 +59,7 @@ module "alb" {
   lb_listner_default_action = "forward"
   lb_https_listner_port     = 443
   lb_https_listner_protocol = "HTTPS"
-  dev_proj_1_acm_arn        = module.aws_ceritification_manager.dev_proj_2_acm_arn
+  dev_proj_2_acm_arn        = module.aws_ceritification_manager.dev_proj_2_acm_arn
   lb_target_group_attachment_port = 5000
 }
 
