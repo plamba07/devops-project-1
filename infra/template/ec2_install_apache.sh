cf4a165e4de50933a61b57da3e333f65f22fac03
plamba07 <prekshalamba1603@gmail.com>
2025-07-12 15:06:14 +0530

    Update ec2_install_apache.sh

--- infra/template/ec2_install_apache.sh
+++ infra/template/ec2_install_apache.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
-# Log everything
 exec > /var/log/user-data.log 2>&1
 set -x
 
 cd /home/ubuntu
 
-# Update packages and install dependencies
+# Update and install dependencies
 sudo apt update -y
-sudo apt install -y python3 python3-pip python3-venv git
+sudo apt install -y python3 python3-venv git
 
-# Clone the app
+# Clone the repo
 git clone https://github.com/rahulwagh/python-mysql-db-proj-1.git
 cd python-mysql-db-proj-1
 
-# Create and activate a virtual environment
+# Create and activate virtual environment
 python3 -m venv venv
 source venv/bin/activate
 
-# Install required Python packages
+# Install Python packages inside venv
 ./venv/bin/pip install -r requirements.txt
 
-# Run the app and log output
+# Run the Flask app in the background with logs
 nohup ./venv/bin/python3 app.py > app.log 2>&1 &
