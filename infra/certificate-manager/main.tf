90bdd877a35682773d63414a9509aa8268d580ad
plamba07 <prekshalamba1603@gmail.com>
2025-07-12 13:37:52 +0530

    Update main.tf

--- infra/certificate-manager/main.tf
+++ infra/certificate-manager/main.tf
@@ -2,10 +2,10 @@ variable "domain_name" {}
 variable "hosted_zone_id" {}
 
 output "dev_proj_1_acm_arn" {
-  value = aws_acm_certificate.dev_proj_1_acm_arn.arn
+  value = aws_acm_certificate.dev_proj_2_acm_arn.arn
 }
 
-resource "aws_acm_certificate" "dev_proj_1_acm_arn" {
+resource "aws_acm_certificate" "dev_proj_2_acm_arn" {
   domain_name       = var.domain_name
   validation_method = "DNS"
 
@@ -20,7 +20,7 @@ resource "aws_acm_certificate" "dev_proj_1_acm_arn" {
 
 resource "aws_route53_record" "validation" {
   for_each = {
-    for dvo in aws_acm_certificate.dev_proj_1_acm_arn.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.dev_proj_2_acm_arn.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
